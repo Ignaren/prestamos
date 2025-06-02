9568aa327ec4d4877343f93051cea83d324abfee
igna <ingn35931@gmail.com>
2025-06-01 21:35:03 -0600

    subiendo proyecto prestamos a railway

--- resources/views/movimientos/prestamosAgregarGet.blade.php
+++ resources/views/movimientos/prestamosAgregarGet.blade.php
@@ -7,29 +7,27 @@
 
 <form method="post" action="{{ url('/movimientos/prestamos/agregar') }}">
     @csrf
-    <!-- sirve para validar que la petición de los datos enviados provengan del formulario actual peticionado -->
 
     <div class="form-group mb-3">
-            <label for="nombre">Empleado:</label>
-            <select class="form-select" name="id_empleado" required autofocus>
-                <!-- muestra el nombre del empleado pero se guarda el id_empleado -->
-                @foreach($empleados as $empleado)
-                    <option value="{{ $empleado->id_empleado }}">
-                        {{ $empleado->nombre }}
-                    </option>
-                @endforeach
-            </select>
+        <label for="fk_id_empleado">Empleado:</label>
+        <select class="form-select" name="fk_id_empleado" id="fk_id_empleado" required autofocus>
+            <option value="">Selecciona un empleado</option>
+            @foreach($empleados as $empleado)
+                <option value="{{ $empleado->id_empleado }}">
+                    {{ $empleado->nombre }}
+                </option>
+            @endforeach
+        </select>
     </div>
     <div class="row">
-    <div class="form-group mb-3 col-2">
-            <label for="fecha solicitud">Fecha de solicitud</label>
+        <div class="form-group mb-3 col-2">
+            <label for="fecha_solicitud">Fecha de solicitud</label>
             <input type="date" name="fecha_solicitud" id="fecha_solicitud" class="form-control" required>
-    </div>
-    <div class="form-group mb-3 col-2">
+        </div>
+        <div class="form-group mb-3 col-2">
             <label for="monto">Monto</label>
             <input type="number" name="monto" id="monto" class="form-control" required>
         </div>
-
         <div class="form-group mb-3 col-2">
             <label for="plazo">Plazo (meses)</label>
             <input type="number" min="1" max="24" name="plazo" id="plazo" class="form-control" required>
@@ -37,36 +35,29 @@
                 Ingresa un número válido
             </div>
         </div>
-
         <div class="form-group mb-3 col-2">
             <label for="tasa_mensual">Tasa mensual</label>
-            <input value="1"type="number" name="tasa_mensual" id="tasa_mensual" class="form-control" required>
+            <input value="1" type="number" name="tasa_mensual" id="tasa_mensual" class="form-control" required>
         </div>
-
         <div class="form-group mb-3 col-2">
-            <label for="fecha_inicio_descuento">Fecha inicio descuento</label>
+            <label for="fecha_ini_desc">Fecha inicio descuento</label>
             <input type="date" name="fecha_ini_desc" id="fecha_ini_desc" class="form-control" required>
         </div>
-
         <div class="form-group mb-3 col-2">
             <label for="fecha_fin_desc">Fecha fin descuento</label>
-            <input readonly type="date" name="fecha_fin_descuento" id="fecha_fin_desc" class="form-control" required>
+            <input readonly type="date" name="fecha_fin_desc" id="fecha_fin_desc" class="form-control" required>
         </div>
     </div>
-
 
     <div class="row">
         <div class="form-group mb-3 col-2">
             <label for="pago_fijo_cap">Pago fijo capital</label>
             <input readonly type="number" step="0.01" name="pago_fijo_cap" id="pago_fijo_cap" class="form-control" required>
         </div>
-
         <div class="form-group mb-3 col-2">
-            <label for="fecha aprobacion">Fecha de aprobación</label>
+            <label for="fecha_aprob">Fecha de aprobación</label>
             <input type="date" name="fecha_aprob" id="fecha_aprob" class="form-control" required>
         </div>
-
-
         <div class="form-group mb-3 col-2">
             <label for="estado">Estado</label>
             <select name="estado" id="estado" class="form-control" required>
@@ -90,12 +81,8 @@
     let inpmonto=document.getElementById("monto");
     let inpplazo=document.getElementById("plazo");
     let inppagofijocap=document.getElementById("pago_fijo_cap");
-    let inpsaldoactual=document.getElementById("saldo");
     let inpfechainidesc=document.getElementById("fecha_ini_desc");
     let inpfechafindesc=document.getElementById("fecha_fin_desc");
-    inpmonto.addEventListener("input",function(){
-        inpsaldoactual.value=inpmonto.value;
-    })
     function calculopagofijocapital(){
         if (!inpmonto.value || !inpplazo.value) {
             return
